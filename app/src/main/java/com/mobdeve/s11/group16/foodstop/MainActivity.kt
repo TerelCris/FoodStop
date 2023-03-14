fb1ae686254cbcef39327f849c0397cd0b9cff07
TerelCris <terel_lean_crisostomo@dlsu.edu.ph>
2023-03-14 15:27:54 +0800

    Update file

--- app/src/main/java/com/mobdeve/s11/group16/foodstop/MainActivity.kt
+++ app/src/main/java/com/mobdeve/s11/group16/foodstop/MainActivity.kt
@@ -2,10 +2,20 @@ package com.mobdeve.s11.group16.foodstop
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    private val recipeList: ArrayList<Recipe> = DataHelper.initializeData()
+
+    private lateinit var recyclerView: RecyclerView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        this.recyclerView = findViewById(R.id.recyclerView)
+        this.recyclerView.adapter = MyAdapter(this.recipeList)
+        this.recyclerView.layoutManager = LinearLayoutManager(this)
     }
 }
