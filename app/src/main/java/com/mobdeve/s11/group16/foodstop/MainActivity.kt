4b5269160d6901790ed2af254d309ec3b3d460e1
TerelCris <terel_lean_crisostomo@dlsu.edu.ph>
2023-03-14 23:15:06 +0800

    updated Intent for Main Activity

--- app/src/main/java/com/mobdeve/s11/group16/foodstop/MainActivity.kt
+++ app/src/main/java/com/mobdeve/s11/group16/foodstop/MainActivity.kt
@@ -1,18 +1,30 @@
 package com.mobdeve.s11.group16.foodstop
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.mobdeve.s11.group16.foodstop.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private val recipeList: ArrayList<Recipe> = DataHelper.initializeData()
 
     private lateinit var recyclerView: RecyclerView
+    private lateinit var adapter: MyAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val viewBinding : ActivityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
+
+        viewBinding.recyclerView.setOnClickListener(View.OnClickListener {
+            val intent : Intent = Intent(this@MainActivity, PostActivity::class.java)
+            this.startActivity(intent)
+        })
 
         this.recyclerView = findViewById(R.id.recyclerView)
         this.recyclerView.adapter = MyAdapter(this.recipeList)
