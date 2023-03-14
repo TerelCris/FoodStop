15b9ab158410c61eabc6c7a10868e4a677972c02
TerelCris <terel_lean_crisostomo@dlsu.edu.ph>
2023-03-14 11:30:20 +0800

    added MyViewHolder and updated rv_layout

--- app/src/main/res/MyViewHolder.kt
+++ app/src/main/res/MyViewHolder.kt
@@ -1,2 +1,16 @@
-class MyViewHolder {
+package com.mobdeve.s11.group16.foodstop
+
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+class MyViewHolder(itemView : View): ViewHolder(itemView) {
+    private val iv_cover: ImageView = itemView.findViewById(R.id.iv_cover)
+    private val tv_category: TextView = itemView.findViewById(R.id.tv_category)
+    private val tv_title: TextView = itemView.findViewById(R.id.tv_title)
+    private val tv_author:TextView = itemView.findViewById(R.id.tv_author)
+    private val tv_date:TextView = itemView.findViewById(R.id.tv_date)
+    private val fab_fav:FloatingActionButton = itemView.findViewById(R.id.fab_fav)
 }
