4a2009e628e830b4f0842ec02fa32a48bef98dc0
TerelCris <terel_lean_crisostomo@dlsu.edu.ph>
2023-03-14 13:06:12 +0800

    Added MyAdapter

--- app/src/main/res/MyAdapter.kt
+++ app/src/main/res/MyAdapter.kt
@@ -1,4 +1,27 @@
 package com.mobdeve.s11.group16.foodstop
 
-class MyAdapter {
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class MyAdapter(private val data: ArrayList<Recipe>) : RecyclerView.Adapter<MyViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val view = inflater.inflate(R.layout.rv_layout, parent, false)
+
+        Log.d("My Adapter", "OnCreateView was called")
+
+        return MyViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        holder.bindData(data[position])
+
+        Log.d("My Adapter", "OnBindView was called; Position $position")
+    }
+
+    override fun getItemCount(): Int {
+        return data.size
+    }
 }
