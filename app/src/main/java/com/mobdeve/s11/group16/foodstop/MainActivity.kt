5976bb98e4b751af7dec02cbb37a3b648842d85e
TerelCris <terel_lean_crisostomo@dlsu.edu.ph>
2023-03-15 12:42:23 +0800

    updated MyAdapter and MyViewHolder

--- app/src/main/java/com/mobdeve/s11/group16/foodstop/MainActivity.kt
+++ app/src/main/java/com/mobdeve/s11/group16/foodstop/MainActivity.kt
@@ -1,19 +1,34 @@
 package com.mobdeve.s11.group16.foodstop
 
+import android.app.Instrumentation.ActivityResult
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.mobdeve.s11.group16.foodstop.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    companion object{
+        private val data = ArrayList<Recipe>()
+    }
 
     private val recipeList: ArrayList<Recipe> = DataHelper.initializeData()
 
     private lateinit var recyclerView: RecyclerView
     private lateinit var adapter: MyAdapter
+
+    private val postActivityLauncher = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()) { result: androidx.activity.result.ActivityResult ->
+
+        if(result.resultCode == RESULT_OK){
+            val position = result.data?.getIntExtra(PostActivity.POSITION_KEY, 0)!!
+            this.adapter.notifyDataSetChanged()
+        }
+
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,7 +42,8 @@ class MainActivity : AppCompatActivity() {
         })
 
         this.recyclerView = findViewById(R.id.recyclerView)
-        this.recyclerView.adapter = MyAdapter(this.recipeList)
+        this.recyclerView.adapter = MyAdapter
+        this.adapter = (MyAdapter(data, postActivityLauncher))
         this.recyclerView.layoutManager = LinearLayoutManager(this)
     }
 }
--- app/src/main/java/com/mobdeve/s11/group16/foodstop/MyAdapter.kt
+++ app/src/main/java/com/mobdeve/s11/group16/foodstop/MyAdapter.kt
@@ -5,11 +5,12 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.ActivityResultLauncher
 import androidx.recyclerview.widget.RecyclerView
 import com.mobdeve.s11.group16.foodstop.databinding.PostLayoutBinding
 import com.mobdeve.s11.group16.foodstop.databinding.RvLayoutBinding
 
-class MyAdapter(private val data: ArrayList<Recipe>) : RecyclerView.Adapter<MyViewHolder>() {
+class MyAdapter(private val data: ArrayList<Recipe>, private val myActivityResultLauncher: ActivityResultLauncher<Intent>) : RecyclerView.Adapter<MyViewHolder>() {
 
     companion object {
         const val KEY_NAME = "KEY_NAME"
@@ -25,11 +26,13 @@ class MyAdapter(private val data: ArrayList<Recipe>) : RecyclerView.Adapter<MyVi
         myViewHolder.itemView.setOnClickListener {
             val intent : Intent = Intent(myViewHolder.itemView.context, PostActivity::class.java)
 
-            intent.putExtra(PostActivity.IMAGE_KEY, itemViewBinding.ivCover.setImageResource(R.drawable.onepotchickenrice))
+            //intent.putExtra(PostActivity.IMAGE_KEY, itemViewBinding.ivCover.setImageResource(R.drawable.onepotchickenrice))
             intent.putExtra(PostActivity.USER_KEY, itemViewBinding.tvAuthor.text.toString())
             intent.putExtra(PostActivity.TITLE_KEY, itemViewBinding.tvTitle.text.toString())
             intent.putExtra(PostActivity.DATE_KEY, itemViewBinding.tvDate.text.toString())
             intent.putExtra(PostActivity.POSITION_KEY, myViewHolder.adapterPosition)
+
+            this.myActivityResultLauncher.launch(intent)
         }
 
         Log.d("My Adapter", "OnCreateView was called")
