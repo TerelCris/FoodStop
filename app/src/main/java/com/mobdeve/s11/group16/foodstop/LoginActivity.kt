8695bb9157835d2e5f4a473326e433f8d16e22a6
TerelCris <terel_lean_crisostomo@dlsu.edu.ph>
2023-03-16 22:05:18 +0800

    Added LoginActivity

---
+++ app/src/main/java/com/mobdeve/s11/group16/foodstop/LoginActivity.kt
@@ -0,0 +1,46 @@
+package com.mobdeve.s11.group16.foodstop
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.viewbinding.ViewBinding
+import com.mobdeve.s11.group16.foodstop.databinding.LoginLayoutBinding
+
+class LoginActivity : AppCompatActivity() {
+    companion object{
+        const val USERNAME_KEY = "USERNAME_KEY"
+        const val PASSWORD_KEY = "PASSWORD_KEY"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val viewBinding : LoginLayoutBinding = LoginLayoutBinding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
+
+        viewBinding.loginbtn.setOnClickListener(View.OnClickListener {
+            if(viewBinding.username.toString().isNotEmpty() && viewBinding.password.toString().isNotEmpty()){
+                val intent : Intent = Intent(this@LoginActivity, MainActivity::class.java)
+
+                intent.putExtra(LoginActivity.USERNAME_KEY, viewBinding.username.text.toString())
+                intent.putExtra(LoginActivity.PASSWORD_KEY, viewBinding.password.text.toString())
+
+                setResult(Activity.RESULT_OK, intent)
+
+                finish()
+            }
+            else{
+                Toast.makeText(
+                    this@LoginActivity,
+                    "Please complete all of the required fields",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        })
+
+    }
+}
