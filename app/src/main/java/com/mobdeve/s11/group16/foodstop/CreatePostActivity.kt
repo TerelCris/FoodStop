be0ab62b71f8be8634935a390e0541f89496d2f3
TerelCris <terel_lean_crisostomo@dlsu.edu.ph>
2023-03-29 21:57:42 +0800

    Updated Create Post Activity

--- app/src/main/java/com/mobdeve/s11/group16/foodstop/CreatePostActivity.kt
+++ app/src/main/java/com/mobdeve/s11/group16/foodstop/CreatePostActivity.kt
@@ -10,15 +10,21 @@ import android.widget.Toast
 import androidx.activity.result.ActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import com.mobdeve.s11.group16.foodstop.databinding.CreatePostLayoutBinding
 import com.mobdeve.s11.group16.foodstop.databinding.PostLayoutBinding
 import com.mobdeve.s11.group16.foodstop.databinding.RvLayoutBinding
+import com.mobdeve.s11.group16.foodstop.databinding.SignupLayoutBinding
 import com.squareup.picasso.Picasso
 
 class CreatePostActivity : AppCompatActivity() {
-    private lateinit var viewBinding : RvLayoutBinding
+    private lateinit var rvBinding : RvLayoutBinding
     private lateinit var postBinding : PostLayoutBinding
     private var addImage: Uri? = null
+    private val databaseReference = FirebaseDatabase.getInstance().getReferenceFromUrl("https://foodstop-9c45c-default-rtdb.firebaseio.com")
 
     private val createPostResultLauncher = registerForActivityResult(
         ActivityResultContracts.StartActivityForResult()) { result: ActivityResult ->
@@ -26,7 +32,7 @@ class CreatePostActivity : AppCompatActivity() {
             try {
                 if (result.data != null) {
                     addImage = result.data!!.data
-                    Picasso.get().load(addImage).into(viewBinding.ivCover)
+                    Picasso.get().load(addImage).into(rvBinding.ivCover)
                     Picasso.get().load(addImage).into(postBinding.postIv)
                 }
             } catch (exception: Exception) {
@@ -38,6 +44,9 @@ class CreatePostActivity : AppCompatActivity() {
         val viewBinding : CreatePostLayoutBinding = CreatePostLayoutBinding.inflate(layoutInflater)
         setContentView(viewBinding.root)
 
+        val userBinding : PostLayoutBinding = PostLayoutBinding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
+
         viewBinding.ibAdd.setOnClickListener(View.OnClickListener {
             val i = Intent()
             i.type = "Post Image/*"
@@ -46,25 +55,35 @@ class CreatePostActivity : AppCompatActivity() {
         })
 
         viewBinding.postBtn.setOnClickListener(View.OnClickListener {
-            if(viewBinding.postTitleEt.text.toString().isNotEmpty() && viewBinding.postDescriptionEt.text.toString().isNotEmpty() && viewBinding.postIngredientEt.text.toString().isNotEmpty() && viewBinding.postProcedureEt.text.toString().isNotEmpty()){
-                val intent : Intent = Intent()
+            var image = viewBinding.ibAdd.text.toString()
+            var title = viewBinding.postTitleEt.text.toString()
+            var description = viewBinding.postDescriptionEt.text.toString()
+            var ingredient = viewBinding.postIngredientEt.text.toString()
+            var procedure = viewBinding.postProcedureEt.text.toString()
+            var username = userBinding.userTv.text.toString()
 
-                intent.putExtra(Keys.TITLE_KEY.name, viewBinding.postTitleEt.text.toString())
-                intent.putExtra(Keys.DESCRIPTION_KEY.name, viewBinding.postDescriptionEt.text.toString())
-                intent.putExtra(Keys.INGREDIENT_KEY.name, viewBinding.postIngredientEt.text.toString())
-                intent.putExtra(Keys.PROCEDURE_KEY.name, viewBinding.postProcedureEt.text.toString())
 
-                setResult(Activity.RESULT_OK, intent)
-
-                finish()
+            if(image.isEmpty() && title.isEmpty() && description.isEmpty() && ingredient.isEmpty() && procedure.isEmpty() && username.isEmpty()){
+                Toast.makeText(this@CreatePostActivity, "Please answer all required fields", Toast.LENGTH_SHORT).show()
+                return@OnClickListener
             }
 
             else{
-                Toast.makeText(
-                    this@CreatePostActivity,
-                    "Please complete all of the required fields",
-                    Toast.LENGTH_LONG
-                ).show()
+                databaseReference.child("Users").addListenerForSingleValueEvent(object : ValueEventListener {
+                    override fun onDataChange(dataSnapshot: DataSnapshot) {
+                        databaseReference.child("Posts").child(username).child("Image").setValue(image)
+                        databaseReference.child("Users").child(username).child("Title").setValue(title)
+                        databaseReference.child("Users").child(username).child("Description").setValue(description)
+                        databaseReference.child("Users").child(username).child("Ingredient").setValue(ingredient)
+                        databaseReference.child("Users").child(username).child("Procedure").setValue(procedure)
+
+                        finish()
+                    }
+
+                    override fun onCancelled(databaseError: DatabaseError) {
+                    }
+                })
+                finish()
             }
         })
     }
